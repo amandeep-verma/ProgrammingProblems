25088d0a999593270dedfd6812485a7561707bb5
amandeep-verma <39786879+amandeep-verma@users.noreply.github.com>
2020-05-24 19:52:18 -0400

    upload

--- addBinaryString.java
+++ addBinaryString.java
@@ -7,78 +7,57 @@
 public class addBinaryString {
 
 	public static void main(String[] args) {
-		
-		String abc=addBinary("11101","101");
 
-				
-		
+		String abc = addBinary("11101", "101");
+
 		System.out.println(abc);
 
 	}
-	
+
 	public static String addBinary(String a, String b) {
-		
+
 		StringBuilder res = new StringBuilder();
-		
-		
-		if(a.length()<b.length())
-		{
-			String temp=a;
-			a=b;
-			b=temp;
+
+		if (a.length() < b.length()) {
+			String temp = a;
+			a = b;
+			b = temp;
 		}
-		int diff=a.length()-b.length();
-				
-		int carry=0;
-		for(int i=a.length()-1;i>=0;i--)
-		{
-			int ValueAta=(a.charAt(i)-'0');
-			
-			int ValueAtb=0;
-			
-			if(i-diff>-1)
-			{
-				ValueAtb= (b.charAt(i-diff)-'0');
+		int diff = a.length() - b.length();
+
+		int carry = 0;
+		for (int i = a.length() - 1; i >= 0; i--) {
+			int ValueAta = (a.charAt(i) - '0');
+
+			int ValueAtb = 0;
+
+			if (i - diff > -1) {
+				ValueAtb = (b.charAt(i - diff) - '0');
+			} else {
+				ValueAtb = 0;
 			}
-			else
-			{
-				ValueAtb=0;
-			}
-			
-			
-			int sum=  (ValueAta+ValueAtb+carry) ;
-			if(sum==3)
-			{
+
+			int sum = (ValueAta + ValueAtb + carry);
+			if (sum == 3) {
 				res.append(1);
-				carry=1;
-			}
-			else if(sum==2)
-			{
+				carry = 1;
+			} else if (sum == 2) {
 				res.append(0);
-				carry=1;
-			}
-			else if(sum==1)
-			{
+				carry = 1;
+			} else if (sum == 1) {
 				res.append(1);
-				carry=0;
-			}
-			else
-			{
+				carry = 0;
+			} else {
 				res.append(0);
-				carry=0;
+				carry = 0;
 			}
-			
+
 		}
-		if(carry==1)
-		{
+		if (carry == 1) {
 			res.append(1);
 		}
-		
-		return res.reverse().toString();
-    }
-    
-    
 
-	
+		return res.reverse().toString();
+	}
 
 }
